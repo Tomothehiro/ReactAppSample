8a465fe2b89a74fd940d00d475e6699640186825
tomo <tomonouchi@hotmail.com>
2019-02-13 23:18:51 -0800

    install faker

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
+// Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Create a React component
+const App = () => {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    return (
+        <div className="ui container comments">
+          <div className="comment">
+            <a href="/" className="avatar">
+              <img alt="avatar" />
+            </a>
+            <div className="content">
+              <a href="/" className="author">
+                Sam
+              </a>
+              <div className="metadata">
+                <span className="date">Today at 6:00PM</span>
+              </div>
+              <div className="text">Nice blog post!</div>
+            </div>
+          </div>
+        </div>
+    );
+}
+
+// Take the React component and insert it into index.html <div id="root"></div>
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
