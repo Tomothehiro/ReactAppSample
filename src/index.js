b28b01baf4d1edb6da987f73beca3c1bc0e8c061
tomo <tomonouchi@hotmail.com>
2019-02-14 21:48:57 -0800

    Update JSX

--- src/index.js
+++ src/index.js
@@ -1,26 +1,28 @@
 // Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 // Create a React component
 const App = () => {
 
     return (
         <div className="ui container comments">
-          <div className="comment">
-            <a href="/" className="avatar">
-              <img alt="avatar" />
-            </a>
-            <div className="content">
-              <a href="/" className="author">
-                Sam
-              </a>
-              <div className="metadata">
-                <span className="date">Today at 6:00PM</span>
+          <ApprovalCard>
+              <div>
+                Are you sure you want to do this?
               </div>
-              <div className="text">Nice blog post!</div>
-            </div>
-          </div>
+          </ApprovalCard>
+          <ApprovalCard>
+            <CommentDetail author="Sam" />
+          </ApprovalCard>
+          <ApprovalCard>
+            <CommentDetail author="Tom" />
+          </ApprovalCard>
+          <ApprovalCard>
+            <CommentDetail author="Bil" />
+          </ApprovalCard>
         </div>
     );
 }
